050f2562c8c42fe466c83181f61f2df67ebf277a
mistermicheels <22038058+mistermicheels@users.noreply.github.com>
2020-04-28 02:34:33 +0200

    improve fontawesome loading by using cdnjs

--- _website/font-awesome-plugin.js
+++ _website/font-awesome-plugin.js
@@ -8,7 +8,14 @@ module.exports = function(_context, _options) {
               tagName: 'link',
               attributes: {
                 rel: 'stylesheet',
-                href: '/lib/fontawesome-free-5.13.0-web/css/all.min.css',
+                
+                // using cdnjs means it's very likely that the user already has this (and the fonts) cached
+                // since the files they serve never change, they can instruct the browser to cache them as long as possible
+
+                // on Netlify, even if the user visited our site before, the browser would need to check the ETag with the server
+                // see https://www.netlify.com/blog/2017/02/23/better-living-through-caching/
+
+                href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css',
               },
             },
           ]
